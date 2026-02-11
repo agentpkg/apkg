9b199d44095e9eccd26e60e5f0b28e9a62009ad3
Calum Murray <cmurray@redhat.com>
2026-02-11 14:54:42 -0500

    fix: cmd is not gitignored
    Signed-off-by: Calum Murray <cmurray@redhat.com>

---
+++ cmd/apkg/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/agentpkg/agentpkg/pkg/cmd"
+	_ "github.com/agentpkg/agentpkg/pkg/projector/claudecode"
+	_ "github.com/agentpkg/agentpkg/pkg/projector/gemini"
+)
+
+func main() {
+	cmd.Execute()
+}
